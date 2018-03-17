38e7ba7f1ba6916b44031a57206c3547675cb8eb
irynaleskiv1995 <iryna.leskiv@ralabs.org>
2018-03-17 16:24:28 +0200

    added barcode reader

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { PopoverController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 import { PopoverPage } from '../about-popover/about-popover';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+    selector: 'page-about',
+    templateUrl: 'about.html'
 })
 export class AboutPage {
-  conferenceDate = '2047-05-17';
+    conferenceDate = '2047-05-17';
 
-  constructor(public popoverCtrl: PopoverController) { }
+    constructor(public popoverCtrl: PopoverController,
+                private barcodeScanner: BarcodeScanner,) { }
 
-  presentPopover(event: Event) {
-    let popover = this.popoverCtrl.create(PopoverPage);
-    popover.present({ ev: event });
-  }
+    presentPopover(event: Event) {
+        let popover = this.popoverCtrl.create(PopoverPage);
+        popover.present({ ev: event });
+    }
+
+    scan() {
+        this.barcodeScanner.scan().then((barcodeData) => {
+            // this.barcodeScanner.encode(barcodeData.format, '111111').then((data) => {
+            //     console.log('data', data);
+            // });
+            console.log('barcodeData', barcodeData);
+        }, (err) => {
+            console.log('err', err);
+        });
+    }
 }
